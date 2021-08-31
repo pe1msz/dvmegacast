aa5e3fa5b12c7a8c2ce7c646ecb5554a831eb210
root <root@pi-star>
2021-08-31 09:52:07 +0200

    fix in scripts

--- CAST.sh
+++ CAST.sh
@@ -1,10 +1,9 @@
 #bin
 #bash
 
-sudo wget https://github.com/pe1msz/cast415/blob/master/update.zip
-sudo unzip update.zip -d /tmp
-sudo chmod +x /tmp
-sudo /tmp/test.sh
+sudo chmod +x *
+cp *.sh /usr/local/sbin
+
 
 
 
