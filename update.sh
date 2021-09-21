50516158e6922d225cbb2d6addcb47029ae926c7
root <root@pi-star>
2021-09-21 14:18:58 +0200

    new mods

---
+++ update.sh
@@ -0,0 +1,8 @@
+#bin
+#bash
+
+sudo cp confirm_main.html /var/www/dashboard/admin/fw
+sudo cp confirm_radio.html /var/www/dashboard/admin/fw
+sudo cp confirm_display.html /var/www/dashboard/admin/fw
+sudo cp index.php /var/www/dashboard/admin/fw
+
