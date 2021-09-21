cc489cee4a3f6cf03185f608b4eb23dba15a5bc2
root <root@pi-star>
2021-09-21 14:35:19 +0200

    removed factory def in configure

--- upcast.sh
+++ upcast.sh
@@ -5,4 +5,5 @@ sudo cp confirm_main.html /var/www/dashboard/admin/fw
 sudo cp confirm_radio.html /var/www/dashboard/admin/fw
 sudo cp confirm_display.html /var/www/dashboard/admin/fw
 sudo cp index.php /var/www/dashboard/admin/fw
+sudo cp configure.php /var/www/dashboard/admin
 
