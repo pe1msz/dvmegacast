29a9c03992574b0dc380e5c35e87e36a0b8a49b3
root <root@pi-star>
2021-11-03 09:40:26 +0100

    added full folder

--- upcast.sh
+++ upcast.sh
@@ -3,6 +3,7 @@
 
 sudo mount -o remount,rw /
 
+sudo unzip -o /var/cast/fwfolder.zip -d /var/www/dashboard/admin/.
 sudo cp /var/cast/check_update.php /var/www/dashboard/admin/fw
 sudo cp /var/cast/confirm_main.html /var/www/dashboard/admin/fw
 sudo cp /var/cast/confirm_radio.html /var/www/dashboard/admin/fw
@@ -11,7 +12,6 @@ sudo cp /var/cast/index.php /var/www/dashboard/admin/fw
 sudo cp /var/cast/configure.php /var/www/dashboard/admin
 sudo cp /var/cast/header-menu.inc /var/www/dashboard/admin/expert
 sudo unzip -o /var/cast/lang.zip -d /var/www/dashboard/admin/lang
-
 sudo cp /var/cast/*.sh /usr/local/sbin
 sudo cp /var/cast/rc.local /etc
 
