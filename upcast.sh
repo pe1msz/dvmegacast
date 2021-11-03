6d984e3e5ed99797488bee013a8e8b291045105b
root <root@pi-star>
2021-11-03 09:31:26 +0100

    added memlist fix

--- upcast.sh
+++ upcast.sh
@@ -3,7 +3,8 @@
 
 sudo mount -o remount,rw /
 
-sudo unzip -o /var/cast/fwfolder.zip -d /var/www/dashboard/admin/.
+sudo unzip -o /var/cast/fwfolder.zip -d /var/www/dashboard/admin/
+sudo unzip -o /var/cast/meml.zip -d /var/www/dashboard/admin/
 sudo cp /var/cast/check_update.php /var/www/dashboard/admin/fw
 sudo cp /var/cast/confirm_main.html /var/www/dashboard/admin/fw
 sudo cp /var/cast/confirm_radio.html /var/www/dashboard/admin/fw
