f3d087c6ce01e0a844a0c58f1f28b8996ed94ccc
root <root@pi-star>
2022-02-01 08:55:37 +0100

    folder fix

--- upcast.sh
+++ upcast.sh
@@ -12,7 +12,7 @@ sudo cp /var/cast/confirm_display.html /var/www/dashboard/admin/fw
 sudo cp /var/cast/index.php /var/www/dashboard/admin/fw
 sudo cp /var/cast/configure.php /var/www/dashboard/admin
 sudo cp /var/cast/header-menu.inc /var/www/dashboard/admin/expert
-sudo chown -R www-data:www-data /var/www/dashboard/admin/expert/fw
+sudo chown -R www-data:www-data /var/www/dashboard/admin/fw
 sudo unzip -o /var/cast/lang.zip -d /var/www/dashboard/admin/lang
 sudo cp /var/cast/*.sh /usr/local/sbin
 sudo cp /var/cast/rc.local /etc
