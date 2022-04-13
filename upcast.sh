61ea3f37f650d5d0b2e4bf01ea7f60d3fabb4696
root <root@pi-star>
2022-04-13 13:23:28 +0200

    added network interfaces fix

--- upcast.sh
+++ upcast.sh
@@ -16,4 +16,4 @@ sudo chown -R www-data:www-data /var/www/dashboard/admin/fw
 sudo unzip -o /var/cast/lang.zip -d /var/www/dashboard/admin/lang
 sudo cp /var/cast/*.sh /usr/local/sbin
 sudo cp /var/cast/rc.local /etc
-
+sudo cp /var/cast/interfaces /etc/network
