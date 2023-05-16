f1a22a21995bf9897d1a28a0a3eccf4763b4c5ba
root <root@pi-star>
2023-05-16 13:48:20 +0200

    fix in file-handle

--- WCON.sh
+++ WCON.sh
@@ -9,16 +9,13 @@
        sudo mount -o remount,rw /;
 
 	FILE=/home/pi-star/config.txt
-	if [ ! -f "$FILE" ]; then
-	echo 'test' >> /home/pi-star/config.txt
-	fi
 	
 
         #Stop Cron (occasionally remounts root as RO - would be bad if it did this at the wrong time....)
         sudo systemctl stop cron.service > /dev/null 2>/dev/null &                   #Cron
 
 
-        sudo sed -i "s/.*/\'$1\'/" /home/pi-star/config.txt
+       echo $1 > $FILE
  
 
 	sudo systemctl start cron.service > /dev/null 2>/dev/null &                   #Cron
--- WSET.sh
+++ WSET.sh
@@ -9,16 +9,11 @@
        sudo mount -o remount,rw /;
 
 	FILE=/home/pi-star/settings.txt
-	if [ ! -f "$FILE" ]; then
-	echo 'test' >> /home/pi-star/settings.txt
-	fi
-	
-
-        #Stop Cron (occasionally remounts root as RO - would be bad if it did this at the wrong time....)
+         #Stop Cron (occasionally remounts root as RO - would be bad if it did this at the wrong time....)
         sudo systemctl stop cron.service > /dev/null 2>/dev/null &                   #Cron
 
 
-        sudo sed -i "s/.*/\'$1\'/" /home/pi-star/settings.txt
+    	echo $1 > $FILE
  
 
 	sudo systemctl start cron.service > /dev/null 2>/dev/null &                   #Cron
