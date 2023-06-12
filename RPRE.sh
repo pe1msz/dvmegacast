b78cac8f6d45af542e68d10538653ced462cdbc9
root <root@pi-star>
2023-06-12 13:55:27 +0200

    added 2 files for presets

---
+++ RPRE.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+input="/home/pi-star/preset.txt"
+((i = 0))
+while IFS= read -r line
+do
+((i=i+1))
+ sudo echo -n  "$line" >/dev/udp/127.0.0.1/40095
+done < "$input"
+
---
+++ WPRE.sh
@@ -0,0 +1,21 @@
+
+#bin
+#bash
+
+# usage: WCON.sh [config-params]
+
+
+# Make the root filesystem writable
+       sudo mount -o remount,rw /;
+
+	FILE=/home/pi-star/preset.txt
+         #Stop Cron (occasionally remounts root as RO - would be bad if it did this at the wrong time....)
+        sudo systemctl stop cron.service > /dev/null 2>/dev/null &                   #Cron
+
+
+    	echo $1 > $FILE
+ 
+
+	sudo systemctl start cron.service > /dev/null 2>/dev/null &                   #Cron
+
+
